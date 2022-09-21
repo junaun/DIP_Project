036dd46876525f99ba86f2016134238d8a2db637
theodore2303 <theodoretan2303@gmail.com>
2022-09-21 16:05:47 +0800

    theo and junbo
    removed unnecessary files

--- cal_jb/android/app/src/main/kotlin/com/example/cal_jb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cal_jb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- calculator_unitconversion/android/app/src/main/kotlin/com/example/calculator_unitconversion/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.calculator_unitconversion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
